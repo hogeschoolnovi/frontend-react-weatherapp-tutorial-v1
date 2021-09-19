ed3f77a110f6f3d22337e6f731142f25787c1f92
novaeeken <nova_eeken@hotmail.com>
2021-09-19 18:28:23 +0200

    Fill in dynamic url

--- src/pages/forecastTab/ForecastTab.js
+++ src/pages/forecastTab/ForecastTab.js
@@ -10,7 +10,7 @@ function ForecastTab({ coordinates }) {
   useEffect(() => {
     async function fetchData() {
       try {
-        const result = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${VOEG HIER LATITUDE TOE}&lon=${VOEG HIER LONGITUDE TOE}&exclude=minutely,current,hourly&appid=${JOUW API KEY}&lang=nl`);
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates?.lon}&exclude=minutely,current,hourly&appid=${apiKey}&lang=nl`);
         console.log(result.data);
       } catch (e) {
         console.error(e);
