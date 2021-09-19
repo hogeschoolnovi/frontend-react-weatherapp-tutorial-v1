4c76bf12efe31c85a95c211b6877e3e2fee807df
novaeeken <nova_eeken@hotmail.com>
2021-09-19 17:16:24 +0200

    Import and install axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
