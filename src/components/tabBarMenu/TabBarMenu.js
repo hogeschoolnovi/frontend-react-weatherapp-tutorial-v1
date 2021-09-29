7ef5863a8b002a8119f68bc1f1b3ae2c9b4e3d3c
novaeeken <nova_eeken@hotmail.com>
2021-09-29 16:10:51 +0200

    Replace anchors with NavLinks

--- src/components/tabBarMenu/TabBarMenu.js
+++ src/components/tabBarMenu/TabBarMenu.js
@@ -7,14 +7,14 @@ function TabBarMenu() {
     <nav className="tab-bar">
       <ul>
         <li>
-          <a className="active" href="/">
+          <NavLink className="active" to="/">
             Vandaag
-          </a>
+          </NavLink>
         </li>
         <li>
-          <a href="/">
+          <NavLink to="/komende-week">
             Komende week
-          </a>
+          </NavLink>
         </li>
       </ul>
     </nav>
