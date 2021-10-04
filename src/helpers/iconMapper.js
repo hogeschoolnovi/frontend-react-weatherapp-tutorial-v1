78c6bd4dfe372113f2200bd5a71db12d8a7ea13b
novaeeken <nova_eeken@hotmail.com>
2021-10-04 16:56:26 +0200

    Import all svg's as react components

--- src/helpers/iconMapper.js
+++ src/helpers/iconMapper.js
@@ -1,1 +1,7 @@
 import React from 'react';
+import { ReactComponent as Sunny } from '../assets/icons/sun.svg';
+import { ReactComponent as Rain } from '../assets/icons/rain.svg';
+import { ReactComponent as Clouds } from '../assets/icons/clouds.svg';
+import { ReactComponent as Snow } from '../assets/icons/snow.svg';
+import { ReactComponent as Wind } from '../assets/icons/wind.svg';
+import { ReactComponent as Drizzle } from '../assets/icons/sun-rain.svg';
